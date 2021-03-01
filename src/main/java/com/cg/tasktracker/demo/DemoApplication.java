ec62a03f20c3fb8216c4487e39a2f1c02dbb7f3d
beingstoic <59771568+beingstoic@users.noreply.github.com>
2021-03-01 15:29:11 +0530

    first commit

---
+++ src/main/java/com/cg/tasktracker/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.cg.tasktracker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = JmxAutoConfiguration.class)
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
